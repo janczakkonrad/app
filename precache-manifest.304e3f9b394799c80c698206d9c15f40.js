48c87c1e8d8935fdda320d463cd85f8c7130b8f1
janczakkonrad <50752633+janczakkonrad@users.noreply.github.com>
2021-01-08 21:07:01 +0100

    Updates

---
+++ precache-manifest.304e3f9b394799c80c698206d9c15f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6773f98f22664d6fde24",
+    "url": "/app/static/css/main.c6c54515.chunk.css"
+  },
+  {
+    "revision": "6773f98f22664d6fde24",
+    "url": "/app/static/js/main.2d203435.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "5ab3b37ca35a42ff2d96",
+    "url": "/app/static/js/2.7092e0b8.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "bf64d4afa7c110ba4272eb5807898cd4",
+    "url": "/app/index.html"
+  }
+];
